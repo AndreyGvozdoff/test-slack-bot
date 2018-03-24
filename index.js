865bdcaa9ae50c184be3c5ec1f0f25f8c9f92683
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-24 14:24:54 +0200

    clear code

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const request = require("superagent");
-const querystring = require("querystring");
-const dataStore = require("./dataStore");
 const _ = require("lodash");
 
 const port = process.env.port || 4000;
-
-const Botkit = require("botkit");
-
-app.use(express.static("views"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   res.send("Test");
