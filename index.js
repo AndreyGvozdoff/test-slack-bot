adbf4138919dc70fdab9a1986b80600dfbb38947
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-24 14:37:06 +0200

    fix heroku port

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const _ = require("lodash");
 
-const port = process.env.port || 4000;
+const port = process.env.port || 5000;
+
+app.use(express.static("views"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   res.send("Test");
