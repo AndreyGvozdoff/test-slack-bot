b5fe8e7c2606226519a25a60c220a10763544b83
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-25 14:54:35 +0300

    test var

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const app = express();
 
 app.get("/", (req, res) => res.send(process.env.app));
 
-app.get(
-  "/callback-subscribes",
-  (req, res) => console.log(JSON.stringify(res))
-  //res.send(res)
-);
+app.get("/callback-subscribes", function(req, res) {
+  console.log(JSON.stringify(res));
+  res.send(res);
+});
 
 app.listen(PORT, () => console.log("App listening on " + PORT));
