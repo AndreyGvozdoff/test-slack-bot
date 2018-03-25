1bedd64d213242f08a37a02ad49346c4abf4a73c
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-25 12:55:11 +0300

    fix heroku

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const app = express();
 
-express().listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get("/", function(req, res) {
+  res.send("Test");
+  //res.send(process.env.mailchimpInstance);
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
