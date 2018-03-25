9f54cf16493e50b1b4edc8089f003185b5d39196
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-25 12:50:12 +0300

    fix heroku dynamic PORT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 express().listen(PORT, () => console.log(`Listening on ${PORT}`));
