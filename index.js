7981829b19a2c1cb7f0d6543101d9bcaae05833c
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-25 13:07:16 +0300

    add route

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", function(req, res) {
   res.send("Test");
-  //res.send(process.env.mailchimpInstance);
+  res.send(process.env.mailchimpInstance);
 });
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get("/callback-subscribes", (req, res) => res.send());
+
+app.listen(PORT, () => console.log("App listening on " + PORT));
