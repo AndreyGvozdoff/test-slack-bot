a2d2f520874ccf8cb0163dab5c5e76f730fe6351
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-25 15:01:05 +0300

    test var

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => res.send(process.env.app));
 
-app.get("/callback-subscribes", function(req, res) {
-  console.log(JSON.stringify(res));
-  res.send(res);
-});
+app.get("/callback-subscribes", (req, res) => res.send(res));
 
 app.listen(PORT, () => console.log("App listening on " + PORT));
