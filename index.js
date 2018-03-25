53e8c02ebc8090e2db91643504ffc4b7250fad7d
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-25 12:37:16 +0300

    test heroku

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-//require("dotenv").config();
 const express = require("express");
-const app = express();
 const path = require("path");
+const PORT = process.env.port || 5000;
 
-const port = process.env.port || 5000;
-
-app.get("/", function(req, res) {
-  res.send("Test");
-  //res.send(process.env.mailchimpInstance);
-});
-
-app.listen(port, () => console.log("App listening on " + port));
+express().listen(PORT, () => console.log(`Listening on ${port}`));
