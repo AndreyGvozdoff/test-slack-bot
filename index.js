9cc4b9e86ba151d1abc46f3c8d542ab9ec8c1f6b
andrew.gvozd <andrew.gvozd@stuzo.com>
2018-03-25 15:54:42 +0300

    add mailchimp api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const request = require("superagent");
 const bodyParser = require("body-parser");
+const Mailchimp = require("mailchimp-api-v3");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -9,42 +10,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let apitoken = process.env.mailchimpApiKey,
-  mailchimpInstance = apitoken.split("-")[1],
-  listUniqueId = process.env.listUniqueId;
+  mailchimp_instance = apitoken.split("-")[1],
+  list_id = process.env.listUniqueId;
 
-app.get("/", (req, res) => res.send(process.env.app));
+const mailchimp = new Mailchimp(apitoken);
 
-app.post("/signin", function(req, res) {
-  request
-    .post(
-      "https://" +
-        mailchimpInstance +
-        ".api.mailchimp.com/3.0/lists/" +
-        listUniqueId +
-        "/members/"
-    )
-    .set("Content-Type", "application/json;charset=utf-8")
-    .set(
-      "Authorization",
-      "Basic " + new Buffer("any:" + mailchimpApiKey).toString("base64")
-    )
-    .send({
-      email_address: req.body.email,
-      status: "subscribed",
-      merge_fields: {
-        FNAME: req.body.firstName,
-        LNAME: req.body.lastName
-      }
+app.get("/", (req, res) => {
+  mailchimp
+    .get(`/lists/${list_id}/members`)
+    .then(function(results) {
+      res.send(results);
     })
-    .end(function(err, response) {
-      if (
-        response.status < 300 ||
-        (response.status === 400 && response.body.title === "Member Exists")
-      ) {
-        res.send("Signed Up!");
-      } else {
-        res.send("Sign Up Failed :(");
-      }
+    .catch(function(err) {
+      res.send(err);
     });
 });
 
